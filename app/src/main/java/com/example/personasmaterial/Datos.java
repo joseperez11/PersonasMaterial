cb658169a0969cc722939353bb46eeb7b65f2031
jose <joseperez730@gmail.com>
2019-07-13 11:28:00 -0500

    clase datos terminada

---
+++ app/src/main/java/com/example/personasmaterial/Datos.java
@@ -0,0 +1,41 @@
+package com.example.personasmaterial;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class Datos {
+    private static String db ="Personas";
+
+    private static DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    public static ArrayList<Persona> personas = new ArrayList<>();
+
+    public static void agregar (Persona p){
+        databaseReference.child(db).child(p.getId()).setValue(p);
+
+    }
+
+
+    public static void editar (Persona p){
+        databaseReference.child(db).child(p.getId()).removeValue(p);
+
+    }
+
+    public static void  eliminar(Persona p){
+        databaseReference.child(db).child(p.getId()).removeValue();
+
+    }
+
+    public static String getId(){
+        return databaseReference.push().getKey();
+    }
+
+    public static void setPersonas(ArrayList<Persona> personas){
+        Datos.personas=personas;
+    }
+    public static  ArrayList<Persona> obtener(){
+        return Datos.personas=personas;
+    }
+
+}
